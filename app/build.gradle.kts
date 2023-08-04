c7c9a5460d47f406a816ca96b794ecd29fa5ccf7
FELIPE GRACIANO BERTANHA DOS SANTOS <22282790@dsn127m1249389.cfp127aluno.local>
2023-08-04 16:38:49 -0300

    adding animations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,4 +70,7 @@ dependencies {
     //Dependencia do Navigation
     implementation("androidx.navigation:navigation-compose:2.6.0")
 
+    // Animacao na navegacao
+    implementation("com.google.accompanist:accompanist-navigation-animation:0.30.1")
+
 }
--- app/src/main/java/br/senai/sp/jandira/navstudies/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/navstudies/MainActivity.kt
@@ -3,19 +3,25 @@ package br.senai.sp.jandira.navstudies
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedContentScope
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.scaleIn
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+//import androidx.navigation.compose.composable
 import br.senai.sp.jandira.navstudies.screens.login.LogintScreen
 import br.senai.sp.jandira.navstudies.screens.menu.MenuScreen
 import br.senai.sp.jandira.navstudies.screens.pedido.PedidosScreen
 import br.senai.sp.jandira.navstudies.screens.perfil.PerfilScreen
 import br.senai.sp.jandira.navstudies.ui.theme.NavStudiesTheme
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
+import com.google.accompanist.navigation.animation.composable
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -24,10 +30,19 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize()
                 ) {
-                    val navController = rememberNavController()
-                    NavHost(
+                    val navController = rememberAnimatedNavController()
+                    AnimatedNavHost(
                         navController = navController,
-                        startDestination = "login"
+                        startDestination = "login",
+                        exitTransition = {
+                            slideOutOfContainer(
+                                AnimatedContentScope.SlideDirection.Start,
+                                tween(1000)
+                                )
+                        },
+                        enterTransition = {
+                            scaleIn(tween(500))
+                        }
                     ) {
                         composable(route = "login"){ LogintScreen(navController)}
                         composable(route = "menu"){ MenuScreen(navController)}
